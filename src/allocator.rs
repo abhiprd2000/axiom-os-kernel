eac11b5f6082ba1e07ec84ef3558da4cd3079ed7
Abhimanyu Prasad <8292aniarc@gmail.com>
2026-06-11 22:31:10 +0000

    fix: correctd HEAP_SIZE comment (8 MiB, not 100 KiB)

--- src/allocator.rs
+++ src/allocator.rs
@@ -9,7 +9,7 @@ use x86_64::{
 };
 
 pub const HEAP_START: usize = 0x_4444_4444_0000;
-pub const HEAP_SIZE: usize = 8 * 1024 * 1024; // 100 KiB
+pub const HEAP_SIZE: usize = 8 * 1024 * 1024; // 8 MiB
 
 #[global_allocator]
 static ALLOCATOR: LockedHeap = LockedHeap::empty();
